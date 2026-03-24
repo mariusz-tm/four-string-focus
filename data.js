82e15fdb0c2b38b268e1a014111b598c0bd7af38
mariusz-tm <s00286100@atu.ie>
2026-03-24 13:37:09 +0000

    Added scripting to index.html
    Added script that randomly changes the "Bassist of the Day" every 00:00 based on user's timezone

---
+++ data.js
@@ -0,0 +1,57 @@
+export const bassists = [
+    {
+        name: "Roger Waters",
+        image: "images/bassists/rogerwaters.jpg",
+        imgCredits: "Photo: Brennan Schnell / Eastscene.com, via Wikimedia Commons.",
+        audio: "audio/money.mp3",
+        information: "Born September 6, 1943, Waters is renowned for his co-founding of the rock band Pink Floyd in 1965. He released 8 albums while in Pink Floyd before leaving in 1985. Notably in 1990, Waters staged one of the largest rock concerts in history, with an attendance of 450,000. Recently, Waters continues his music career solo."
+    },
+
+    {
+        name: "桜井 哲夫 (Sakurai Tetsuo)",
+        image: "images/bassists/sakuraitetsuo.jpg",
+        imgCredits: "Photo: Jazzuality.com, via Flickr.",
+        audio: "audio/midnightrendezvous.mp3",
+        information: "Born November 13, 1957, Sakurai is well famed for his founding of the jazz fusion band Casiopea in 1976, alongside 野呂 一生 (Noro Issei). He released 19 albums while in Casiopea before leaving in 1989. Nowadays, Sakurai is a member of a group called Katsushika Trio alongside 2 other ex-Casiopea members."
+    },
+
+    {
+        name: "Michael Peter Balzary",
+        image: "images/bassists/michaelpeterbalzary.jpg",
+        imgCredits: "Photo: Stefan Brending / 2eight, via Wikimedia Commons.",
+        audio: "audio/peacesells.mp3",
+        information: "6"
+    },
+
+    {
+        name: "Dave Ellefson",
+        image: "images/bassists/daveellefson.jpg",
+        imgCredits: "Photo: Ralph Arvesen / Lostoak.com, via Flickr.",
+        audio: "audio/californication.mp3",
+        information: "7"
+    },
+
+    {
+        name: "Wojtek Pilichowski",
+        image: "images/bassists/wojtekpilichowski.jpg",
+        imgCredits: "Photo: Nietoksyczny, via Wikimedia Commons.",
+        audio: "audio/lateevening.mp3",
+        information: "4"
+    },
+
+    {
+        name: "Kevin Parker",
+        image: "images/bassists/kevinparker.jpg",
+        imgCredits: "Photo: Jorge Gobbi / morrissey, via Flickr.",
+        audio: "audio/thelessiknowthebetter.mp3",
+        information: "Born January 20, 1986, Parker is best known for his musical project Tame Impala, for which he writes, performs, records, and produces the music. He released 5 albums under Tame Impala. Alongside his work for Tame Impala, Parker has collaborated as a producer with artists such as Lady Gaga, Gorillaz, Rihanna, and Travis Scott."
+    },
+
+    {
+        name: "Rafael Navejas",
+        image: "images/bassists/rafaelnavejas.jpg",
+        imgCredits: "Photo: El Sombrerero, via Wikimedia Commons.",
+        audio: "audio/impacto.mp3",
+        information: "5"
+    },
+];
