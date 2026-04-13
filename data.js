51a116b02980fe61225a2596f2b8f1841b2f1153
mariusz-tm <s00286100@atu.ie>
2026-04-13 16:32:18 +0100

    Created final 2 webpages
    Create history.html and locator.html
    Removed credits html link and put a hyperlink to credits to relevant images/audio.

--- data.js
+++ data.js
@@ -3,6 +3,7 @@ export const bassists = [
         name: "Roger Waters",
         image: "images/bassists/r-waters/rogerwatersmain.jpg",
         imgCredits: "Photo: Brennan Schnell / Eastscene.com, via Wikimedia Commons.",
+        creditsLink: "https://commons.wikimedia.org/wiki/File:Roger_Waters_-_The_Wall_in_Ottawa_%287451690376%29.jpg",
         audio: "audio/r-waters/money.mp3",
         information: "Born September 6, 1943, Waters is renowned for his co-founding of the rock band Pink Floyd in 1965. He released 8 albums while in Pink Floyd before leaving in 1985. Notably in 1990, Waters staged one of the largest rock concerts in history, with an attendance of 450,000. Recently, Waters continues his music career solo."
     },
@@ -11,6 +12,7 @@ export const bassists = [
         name: "桜井 哲夫 (Sakurai Tetsuo)",
         image: "images/bassists/s-tetsuo/sakuraitetsuomain.jpg",
         imgCredits: "Photo: Jazzuality.com, via Flickr.",
+        creditsLink: "https://www.flickr.com/photos/jazzuality/8105717785",
         audio: "audio/s-tetsuo/midnightrendezvous.mp3",
         information: "Born November 13, 1957, Sakurai is well famed for his founding of the jazz fusion band Casiopea in 1976, alongside 野呂 一生 (Noro Issei). He released 19 albums while in Casiopea before leaving in 1989. Nowadays, Sakurai is a member of a group called Katsushika Trio alongside 2 other ex-Casiopea members."
     },
@@ -19,6 +21,7 @@ export const bassists = [
         name: "Michael Peter Balzary",
         image: "images/bassists/m-balzary/michaelpeterbalzarymain.jpg",
         imgCredits: "Photo: Stefan Brending / 2eight, via Wikimedia Commons.",
+        creditsLink: "https://commons.wikimedia.org/wiki/File:2016_RiP_Red_Hot_Chili_Peppers_-_Michael_Flea_Balzary_-_by_2eight_-_DSC0085.jpg",
         audio: "audio/m-balzary/californication.mp3",
         information: "Born October 16, 1962, Michael Peter Balzary is best known as the bassist and co-founder of the rock band Red Hot Chili Peppers, for which he writes, performs, and occasionally produces music. He has released 13 albums with the Red Hot Chili Peppers since their formation in 1983. Alongside his work with the band, Balzary has collaborated and recorded with artists such as Johnny Cash, The Mars Volta, Thom Yorke, and Alanis Morissette."
     },
@@ -27,6 +30,7 @@ export const bassists = [
         name: "Dave Ellefson",
         image: "images/bassists/d-ellefson/daveellefsonmain.jpg",
         imgCredits: "Photo: Ralph Arvesen / Lostoak.com, via Flickr.",
+        creditsLink: "https://www.flickr.com/photos/rarvesen/27457608296/",
         audio: "audio/d-ellefson/peacesells.mp3",
         information: "Born November 12, 1964, Dave Ellefson is best known as the bassist and co-founder of the thrash metal band Megadeth, for which he performs and co-writes music. He released 16 albums with Megadeth across his tenures with the band between 1983 and 2021. Alongside his work with Megadeth, Ellefson has collaborated with artists such as Metal Allegiance, Temple of Brutality, F5, and Killing Machine."
     },
@@ -35,6 +39,7 @@ export const bassists = [
         name: "Wojtek Pilichowski",
         image: "images/bassists/w-pilichowski/wojtekpilichowskimain.jpg",
         imgCredits: "Photo: Nietoksyczny, via Wikimedia Commons.",
+        creditsLink: "https://commons.wikimedia.org/wiki/File:Wojciech_Pilichowski_01.05.2022_(cropped).jpg",
         audio: "audio/w-pilichowski/lateevening.mp3",
         information: "Born April 16 1969, Pilichowski is a famed Polish bassist, being recognised as the best Polish bass player through numerous nominations and awards nationally. Recording his first solo album in 1994, Pilichowski has since released 20 solo albums, and over 200 total albums through his collaboration with other artists."
     },
@@ -43,6 +48,7 @@ export const bassists = [
         name: "Kevin Parker",
         image: "images/bassists/k-parker/kevinparkermain.jpg",
         imgCredits: "Photo: Jorge Gobbi / morrissey, via Flickr.",
+        creditsLink: "https://www.flickr.com/photos/morrissey/7823436454/in/photolist-2ocpYQJ-cUYwTm-r2swoi-2kXcFzF-h4YShR-dsp3jF-2kX4TKS-h72HhD-oL7dK6-9A9W7E-oL7sLd-gYQzvF-5CBw5Y-oL7UAM-8VQJ7H-8vkyQr-9BjXZi-gsNEDZ-5CxfNn-wEChZA-5CBvBL-e4CQEb-Jp63d8-cVka6q-87HtqF-oP5ACt-a9H5h8-p6wWTf-oP5fE5-sRNj2M-r2sxiK-KCMK5T-ifshxv-87Htrc-oL9aBw-HsYN5J-odsLx4-oR7zM8-HsYLUs-gqTrRu-odyToX-gqSMPL-fKMMKK-2cNNdwD-2o1t9Si-8voBpS-gqSNWL",
         audio: "audio/k-parker/thelessiknowthebetter.mp3",
         information: "Born January 20, 1986, Parker is best known for his musical project Tame Impala, for which he writes, performs, records, and produces the music. He released 5 albums under Tame Impala. Alongside his work for Tame Impala, Parker has collaborated as a producer with artists such as Lady Gaga, Gorillaz, Rihanna, and Travis Scott."
     },
@@ -51,6 +57,7 @@ export const bassists = [
         name: "Rafael Navejas",
         image: "images/bassists/r-navejas/rafaelnavejasmain.jpg",
         imgCredits: "Photo: El Sombrerero, via Wikimedia Commons.",
+        creditsLink: "https://es.wikipedia.org/wiki/Archivo:Rafael_Navejas.jpg",
         audio: "audio/r-navejas/impacto.mp3",
         information: "Born June 18, 1979, Rafael Navejas is best known as the bassist of the rock band Enjambre, for which he performs and contributes to songwriting. He has released 8 albums with Enjambre since joining the group. Alongside his work with the band, Navejas has collaborated with various artists within the Latin alternative and rock music scenes."
     },
